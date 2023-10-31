d0b160ce891f39c3e2530560340f0bf2b07e0ad3
Anshyadav2208 <144037751+Anshyadav2208@users.noreply.github.com>
2023-10-31 12:18:50 +0530

    Add files via upload

---
+++ Main (11).java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main extends Thread{
+  public  void run()
+  {
+      System.out.println("Welcome to CSIT");
+  }
+  public static void main(String[] args)
+  {
+      
+  
+    Main t1 = new Main();
+    Main t2 = new Main();
+    Main t3 = new Main();
+    t1.start();
+    t2.start();
+    t3.start();
+}
+}
