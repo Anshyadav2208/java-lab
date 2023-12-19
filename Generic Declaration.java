54915d65d623316c5c82e13af89d92cfc7d43e1b
Anshyadav2208 <144037751+Anshyadav2208@users.noreply.github.com>
2023-12-19 12:00:32 +0530

    Add files via upload

---
+++ Generic Declaration.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Ex {
+    public void printArray(String[] s) {
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Ex ex = new Ex();
+        String[] animals = new String[] { "cat", "dog", "cow" };
+        ex.printArray(animals);
+    }
+}
